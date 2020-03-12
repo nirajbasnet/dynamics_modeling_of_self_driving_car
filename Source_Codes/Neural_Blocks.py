2e468a6ff1755b8b67dd87e233db44f6ffb9ecdd
intelaashish <aashish73134@gmail.com>
2020-03-11 19:09:47 -0700

    changed conv_stack module to a general form, added onservation encoder using different modules

--- Source_Codes/Neural_Blocks.py
+++ Source_Codes/Neural_Blocks.py
@@ -5,16 +5,16 @@ import torch.nn as nn, time
 
 class Conv_Stack(nn.Module):
 
-    def __init__(self):
+    def __init__(self, k1=3,c1=1,k2=3,c2=1,k3=3,c3=1,s1=1,s2=1,s3=1,p1=0,p2=0,p3=0):
 
         super(Conv_Stack, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding= 0)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=c1, kernel_size=k1, stride=s1, padding= p1)
         self.leakyrelu_1 = nn.LeakyReLU()
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels= 16, kernel_size=5, stride=1, padding=2) #Size-Expanding convolution
+        self.conv2 = nn.Conv2d(in_channels=c1, out_channels= c2, kernel_size=k2, stride=s2, padding=p2) #Size-Expanding convolution
         self.leakyrelu_2 = nn.LeakyReLU()
-        self.conv3 = nn.Conv2d(in_channels=16, out_channels=64, kernel_size=5, stride=1, padding=0) #op dimension not matched with anything for now
+        self.conv3 = nn.Conv2d(in_channels=c2, out_channels=c3, kernel_size=k3, stride=s3, padding=p3) #op dimension not matched with anything for now
 
-    def forward(self, input): #input dimension 150 X 150 X 1
+    def forward(self, input):
 
         input = self.conv1(input)
         input = self.leakyrelu_1(input)
@@ -22,7 +22,7 @@ class Conv_Stack(nn.Module):
         input = self.leakyrelu_2(input+input_)
         input = self.conv3(input)
 
-        return input #Output dimension is 144 X 144 X 64
+        return input
 
 # space_to_depth operation outputs a copy of the input tensor where values from the height and width dimensions are moved to the depth dimension.
 # It transfers an input size of (None, 38,38,64) to (None, 19,19, 256).
@@ -54,3 +54,23 @@ class SpaceToDepth(nn.Module):
         x = x.view(no_dimen, channel_depth * (self.block_size ** 2), height // self.block_size, width // self.block_size)
         return x
 
+class Observation_Encoder(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.space_to_depth_1 = SpaceToDepth(4)
+        self.conv_stack_1 = Conv_Stack(k1=3, c1=16, k2=5, c2=16, k3=3, c3=64, s1=1, s2=2, s3=1, p1=0,p2=2,p3=0)
+        self.space_to_depth_2 = SpaceToDepth(2)
+        self.conv_stack_2 = Conv_Stack(k1=3, c1=32, k2=5, c2=32, k3=3, c3=64, s1=1, s2=2, s3=1, p1=0, p2=2, p3=0)
+        self.leaky_relu = nn.LeakyReLU()
+
+
+    def forward(self, input):
+        input = self.space_to_depth_1(input)
+        input = self.conv_stack_1(input)
+        input = self.space_to_depth_2(input)
+        input = self.space_to_depth_2(input)
+        input = self.conv_stack_2(input)
+        input = self.leaky_relu(input)
+
+        return input
